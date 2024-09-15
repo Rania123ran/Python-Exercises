c00be22f0a5c93ab92b818757f63210208f40ee6
Rania AKOURROU <149415853+Rania123ran@users.noreply.github.com>
2024-09-15 18:23:40 +0100

    Create fibonnaci_recursion.py

---
+++ fibonnaci_recursion.py
@@ -0,0 +1,9 @@
+n = int(input("saisir un nombre : "))
+def fib(n) : 
+    if n==0 :
+        return 0
+    elif n==1 : 
+        return 1 
+    else : 
+        return fib(n-1)+fib(n-2)
+print("somme fibbonacci de : ",n," est : ",fib(n))b
